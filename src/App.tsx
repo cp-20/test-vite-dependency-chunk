3562546839aa61bf0b632840e9019f4e399a2ed2
cp-20 <n.naoki.1587@gmail.com>
2024-12-20 13:43:50 +0900

    🍣 🍲 🍺 iconify

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import { IconArrowLeft } from '@tabler/icons-react';
+import { Icon } from '@iconify/react';
 import './App.css';
 
 function App() {
@@ -30,6 +31,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
       <IconArrowLeft size={24} />
+      <Icon icon="akar-icons:arrow-left" width="24" height="24" />
     </>
   );
 }
